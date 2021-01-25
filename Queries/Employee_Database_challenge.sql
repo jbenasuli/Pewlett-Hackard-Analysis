b60fedae6731f4fbc3ed2e24d07c44cb37cd4ccc
James Benasuli <jamiebenasuli@gmail.com>
2021-01-25 00:46:18 -0500

    deliverable 1 challenge file commit

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,112 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+
+-- Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+SELECT emp_no, first_name, last_name
+FROM employees
+
+-- Retrieve the title, from_date, and to_date columns from the Titles table.
+SELECT title, from_date, to_date
+FROM titles;
+
+-- Create a new table using the INTO clause.
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles
+
+-- Join both tables on the primary key.
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+
+-- Filter the data on the birth_date column to retrieve the employees who were born between 1952 and 1955. Then, order by the employee number.
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Export the Retirement Titles table from the previous step as retirement_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+    -- Before you export your table, confirm that it looks like this image
+SELECT * FROM retirement_titles;
+
+-- Copy the query from the Employee_Challenge_starter_code.sql and add it to your Employee_Database_challenge.sql file.
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
+
+-- Retrieve the employee number, first and last name, and title columns from the Retirement Titles table. 
+    -- These columns will be in the new table that will hold the most recent title of each employee.
+SELECT emp_no, first_name, last_name, title
+FROM retirement_titles
+
+-- Use the DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+SELECT DISTINCT ON (emp_no) emp_no,
+    first_name,
+    last_name,
+    title
+FROM retirement_titles
+
+-- Create a Unique Titles table using the INTO clause.
+SELECT DISTINCT ON (emp_no) emp_no,
+    first_name,
+    last_name,
+    title
+INTO unique_titles
+FROM retirement_titles
+
+-- Sort the Unique Titles table in ascending order by the employee number and descending order by the last date (i.e. to_date) of the most recent title.
+SELECT DISTINCT ON (emp_no) emp_no,
+    first_name,
+    last_name,
+    title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Export the Unique Titles table as unique_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+    -- Before you export your table, confirm that it looks like this image:
+SELECT * FROM unique_titles;
+
+-- Write another query in the Employee_Database_challenge.sql file to retrieve the number of employees by their most recent job title who are about to retire.
+-- First, retrieve the number of titles from the Unique Titles table.
+SELECT COUNT(title)
+FROM unique_titles
+
+-- Then, create a Retiring Titles table to hold the required information.
+SELECT COUNT(title)
+FROM unique_titles
+INTO retiring_titles
+
+-- Group the table by title, then sort the count column in descending order.
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+-- Export the Retiring Titles table as retiring_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+    -- Before you export your table, confirm that it looks like this image:
+SELECT * FROM retiring_titles;
