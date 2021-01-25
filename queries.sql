7ef6175ca5b47e9766580ea5d2705e9a2b50857c
James Benasuli <jamiebenasuli@gmail.com>
2021-01-24 20:40:51 -0500

    adding new lists and updated queries files

--- queries.sql
+++ queries.sql
@@ -35,7 +35,7 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- viewing newly creating table
+-- viewing newly created table
 SELECT * FROM retirement_info;
 -- Dropping retirment info to recreate new version
 DROP TABLE retirement_info;
@@ -71,7 +71,7 @@ SELECT ri.emp_no,
 FROM retirement_info as ri
 LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no;
--- Use Left Join for retirement_info and dept_emp tables
+-- Use Left Join for retirement_info and dept_emp tables to create current_emp table
 SELECT ri.emp_no,
     ri.first_name,
     ri.last_name,
@@ -102,3 +102,63 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+-- validating salaries.to_date data
+SELECT * FROM salaries;
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+-- drafting new temporary table (emp_info)
+SELECT emp_no,
+    first_name,
+    last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31'); 
+-- creating new table with additional columns, aliases and joins
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+    AND (de.to_date = '9999-01-01');
+-- confirming successful table creating before csv export
+SELECT * FROM emp_info;
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+-- confirming successful table creating before csv export
+SELECT * FROM manager_info;
+-- updating the current_emp table to also include a list of departments
+SELECT ce.emp_no,
+    ce.first_name,
+    ce.last_name,
+    d.dept_name
+INTO dept_info
+FROM current_emp as ce
+    INNER JOIN dept_emp AS de
+        ON (ce.emp_no = de.emp_no)
+    INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no);
+-- confirming successful table creating before csv export
+SELECT * FROM dept_info;
